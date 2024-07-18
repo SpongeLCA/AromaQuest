ea0e9495d4a58f417331c63391d722385dd170c0
Jenna Claudia Andriamanantena <jennaandria@gmail.com>
2024-07-18 15:24:15 +0200

    navbar boostrap

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,8 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema[7.1].define(version: 2024_07_18_065309) do
 
+ActiveRecord::Schema[7.1].define(version: 2024_07_18_101324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
