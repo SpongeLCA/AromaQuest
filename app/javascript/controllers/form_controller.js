8579e62882aafd66a3c3b10a42b63276c74d5202
Solene Baudic <solene.baudic@gmail.com>
2024-07-19 12:38:49 +0200

    Questionnaire finalisé

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -5,6 +5,7 @@ export default class extends Controller {
 
   connect() {
     this.showCurrentStep();
+    this.updateProgressBar();
   }
 
   selectAnswer(event) {
@@ -15,19 +16,29 @@ export default class extends Controller {
     if (currentStep < this.stepTargets.length - 1) {
       this.stepTargets[currentStep].classList.add("hidden");
       this.stepTargets[currentStep + 1].classList.remove("hidden");
+      this.updateProgressBar();
     } else {
       // Affiche les valeurs des champs cachés avant la soumission du formulaire
       this.formTarget.submit();
-      console.log("Champs cachés avant soumission :");
-      Array.from(this.formTarget.elements).forEach((element) => {
-        if (element.type === "hidden") {
-          console.log(`${element.name}: ${element.value}`);
-        }
-      });
-
-
+      // console.log("Champs cachés avant soumission :");
+      // Array.from(this.formTarget.elements).forEach((element) => {
+      //   if (element.type === "hidden") {
+      //     console.log(`${element.name}: ${element.value}`);
+      //   }
+      // });
     }
   }
+
+  previousStep(event) {
+    event.preventDefault();
+    const currentStep = this.currentStepIndex();
+    if (currentStep > 0) {
+      this.stepTargets[currentStep].classList.add("hidden");
+      this.stepTargets[currentStep - 1].classList.remove("hidden");
+      this.updateProgressBar();
+    }
+  }
+
 
   currentStepIndex() {
     return this.stepTargets.findIndex(step => !step.classList.contains("hidden"));
@@ -49,5 +60,12 @@ export default class extends Controller {
     input.name = `result[answer_${stepIndex + 1}]`;
     input.value = answer;
     this.formTarget.appendChild(input);
+  }
+
+  updateProgressBar() {
+    const currentStep = this.currentStepIndex();
+    const totalSteps = this.stepTargets.length;
+    const progressPercent = (currentStep / (totalSteps - 1)) * 100;
+    document.getElementById('progress-bar').style.width = `${progressPercent}%`;
   }
 }
