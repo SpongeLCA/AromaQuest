8ece617d332379a27b2031fe5d50bf3db644e98f
Loic CARPENTIER <carpentier.loic19@gmail.com>
2024-07-23 12:45:49 +0200

    correction

--- db/schema.rb
+++ db/schema.rb
@@ -188,6 +188,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_23_093518) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.boolean "notifications"
+    t.boolean "newsletter"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
